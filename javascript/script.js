362c3e171a2e551b3394f87d0fc2246766663e14
Renan Lucena dos Santos <69248786+RenanLucenaDeveloper@users.noreply.github.com>
2021-07-26 21:37:10 -0300

    Segunda versao

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,25 @@
 const botoesAbrir = document.querySelectorAll('[data-modal="abrir"]');
 const botaofechar = document.querySelector('[data-modal="fechar"]');
 const modalContainer = document.querySelector('[data-modal="container"]');
+const html = document.documentElement;
 
 function abrirModal(e) {
-
     modalContainer.classList.add('ativo');
+    setTimeout(() => {html.addEventListener('click', fecharModal);});
 };
 
 function fecharModal(e) {
     e.preventDefault();
-    modalContainer.classList.remove('ativo');
+
+    if(e.target === modalContainer || e.target === botaofechar) {
+        modalContainer.classList.remove('ativo');
+        html.removeEventListener('click', fecharModal);
+    }
 };
+
+function clickOutside(e) {
+    console.log(e.target);
+}
 
 botoesAbrir.forEach( (i) => { i.addEventListener('click', abrirModal) } );
 botaofechar.addEventListener('click', fecharModal);
